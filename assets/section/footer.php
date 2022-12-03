7361d1ad73d5d76e415410f6607e3abd4e55a361
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2022-12-03 23:53:15 +0100

    removed assets folder

--- assets/section/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="w-screen p-8 bg-white shadow-md flex items-center justify-between gap-4 flex-wrap">
-    <a href="./">
-        <h1 class="font-bold lg:text-3xl text-lg"><span class="font-black text-red-500">E</span>-commerce</h1>
-    </a>
-    <div class="grow flex flex-wrap items-center justify-center">
-        <span class="text-center">Copyright © 2022 All rights reserved | marked</span>
-    </div>
-    <div class="grow flex items-center justify-center gap-4"></div>
-</footer>
