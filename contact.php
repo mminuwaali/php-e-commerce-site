a5b268cd39543489633cc15494f2007b3e242396
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2022-12-03 03:51:49 +0100

    implemented the homepage

---
+++ contact.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Contact Us</title>
+  <?php include("./assets/components/meta.php"); ?>
+</head>
+
+<body>
+  <?php include("./assets/components/header.php"); ?>
+  <?php include("./assets/components/footer.php"); ?>
+</body>
+
+</html>
