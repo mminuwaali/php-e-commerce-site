158f1210cc0cbc30f6a555a1c470bf513dea1d6e
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2022-12-04 22:10:53 +0100

    minor update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+// import './bootstrap';
+
+const slideshow = document.querySelector('#slideshow');
+const slideButtons = slideshow.querySelectorAll('[data-slideshow-count]');
+
+slideButtons.forEach(btn => btn.addEventListener('click', ({ currentTarget: target }) => { slideshow.style.setProperty('--x', Number(target.getAttribute('data-slideshow-count'))) }));
+
+
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,24 +2,27 @@
 
 @section('content')
     <!-- slideshow -->
-    <section id="slideshow"
+    <section id="slideshow" style="--x: 1;"
         class="w-full md:aspect-video bg-green-400 flex justify-start overflow-hidden relative text-white">
         @for ($i = 1; $i <= 3; $i++)
             <div
-                class="grow shrink-0 bg-black py-14 md:py-10 p-10 flex flex-col items-center md:items-start justify-center w-full gap-2 md:gap-10">
+                class="transition-all grow shrink-0 bg-black py-14 md:py-10 p-10 flex flex-col items-center md:items-start justify-center w-full gap-2 md:gap-10 relative -translate-x-[calc(100%*var(--x))]">
+                <img src="{{ url('/images/bannar.jpg') }}" alt="" class="absolute w-full h-full top-0 left-0 z-0">
                 <h2
-                    class="text-[clamp(1.4em,5vw,5em)] w-[max(10em,50%)] capitalize leading-[1] font-bold text-center md:text-left">
+                    class="text-[clamp(1.4em,5vw,5em)] w-[max(10em,50%)] capitalize leading-[1] font-bold text-center md:text-left z-10">
                     a new online shop experience</h2>
-                <p class="w-full md:w-[max(10em,50%)] text-[clamp(0.6em,1vw,5em)]">
+                <p class="w-full md:w-[max(10em,50%)] text-[clamp(0.6em,1vw,5em)] z-10">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a ultricies metus. Sed nec molestie
                     eros. Sed viver ra velit venenatis fermentum luctus.
                 </p>
-                <a href="#" class="border-white border-2 px-8 py-2 text-xs hover:text-black hover:bg-white">shop now</a>
+                <a href="{{ route('categories') }}"
+                    class="border-white border-2 px-8 py-2 text-xs hover:text-black hover:bg-white z-10">shop now</a>
             </div>
         @endfor
         <div class="flex items-center justify-start gap-2 md:gap-4 p-1 px-8 md:p-8 absolute bottom-0 left-0 w-full z-10">
             @for ($i = 1; $i <= 3; $i++)
-                <span class="text-white font-black cursor-pointer hover:text-green-500">0{{ $i }}.</span>
+                <span data-slideshow-count="{{ $i - 1 }}"
+                    class="text-white font-black cursor-pointer hover:text-green-500">0{{ $i }}.</span>
             @endfor
         </div>
     </section>
@@ -35,12 +38,15 @@
     </section>
     <!-- products section -->
     <section id="products" class="flex flex-wrap justify-center gap-8">
-        @for ($i = 0; $i < 12; $i++)
+        @for ($i = 1; $i <= 12; $i++)
             <div class="w-64 h-80 flex flex-col shadow rounded">
-                <div class="grow bg-green-400"></div>
+                <div class="grow bg-green-400 relative">
+                    <img src="{{ url('/images/products/product-' . $i % 8 + 1 . '.jpg') }}" alt=""
+                        class="w-full h-full object-cover absolute" />
+                </div>
                 <div class="w-full px-4 py-8 flex flex-col items-start justify-center">
                     <a href="{{ route('product', ['id' => $i]) }}" class="capitalize font-bold text-gray-500 text-md">
-                        product name
+                        product number {{ $i }}
                     </a>
                     <div class="w-full flex items-center justify-between gap-2">
                         <span>&dollar; {{ $i + 1 }},000</span>
